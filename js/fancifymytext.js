0ecbff6c30b0ef0be632643435d8cd4e340d1dff
SomeOrdinaryGamer <81064130+SomeOrdinaryGamer@users.noreply.github.com>
2022-02-18 17:09:31 -0800

    Update fancifymytext.js

--- js/fancifymytext.js
+++ js/fancifymytext.js
@@ -1,34 +1,37 @@
 function bigger() {  
-  document.getElementById("text").style.fontSize = "24pt";
-  alert("Hello, world!");
-}
-
-function fancy() {  
-  let textField = document.getElementById("text");
-  if(document.getElementById("fancy").checked) {
-//     textField.style.fontSize = "12pt";
-    textField.style.fontWeight = "bold";
-    textField.style.color = "#0000ff";
-    textField.style.textDecoration = "underline";
-    textField.style.fontFamily = "Garamond";
-    document.getElementById("boring").checked = "false";
-}
-
-function boring() {  
-  let textField = document.getElementById("text");
-  if(document.getElementById("boring").checked) {
-    textField.style.fontSize = "12pt";
-    textField.style.fontWeight = "normal";
-    textField.style.color = "#000000";
-    textField.style.textDecoration = "none";
-    textField.style.fontStyle = "Arial";
-    document.getElementById("fancy").checked = "false";
-}
-
-function moo() {  
-  let textField = document.getElementById("text");
-    textField.style.textTransform = "uppercase";
-//     textField.style.textTransform = "capitalize";
-  let parts = textField.value.split(".");
-    textField.value = parts.join("-Moo.");
-}
+    document.getElementById("text").style.fontSize = "24pt";
+    alert("Hello, world!");
+  }
+  
+  function fancy() {  
+    let textField = document.getElementById("text");
+    if(document.getElementById("fancy").checked) {
+  //     textField.style.fontSize = "12pt";
+      textField.style.fontWeight = "bold";
+      textField.style.color = "#0000ff";
+      textField.style.textDecoration = "underline";
+      textField.style.fontFamily = "Garamond";
+      document.getElementById("boring").checked = "false";
+    }
+  }
+  
+  
+  function boring() {  
+    let textField = document.getElementById("text");
+    if(document.getElementById("boring").checked) {
+      textField.style.fontSize = "12pt";
+      textField.style.fontWeight = "normal";
+      textField.style.color = "#000000";
+      textField.style.textDecoration = "none";
+      textField.style.fontStyle = "Arial";
+      document.getElementById("fancy").checked = "false";
+     }
+    }
+  
+  function moo() {  
+    let textField = document.getElementById("text");
+      textField.style.textTransform = "uppercase";
+  //     textField.style.textTransform = "capitalize";
+    let parts = textField.value.split(".");
+      textField.value = parts.join("-Moo.");
+  }
