3fad1f69cfd94b26d934bfb548713ed9965d8345
SomeOrdinaryGamer <81064130+SomeOrdinaryGamer@users.noreply.github.com>
2022-02-18 16:33:12 -0800

    Update fancifymytext.js

--- js/fancifymytext.js
+++ js/fancifymytext.js
@@ -3,27 +3,26 @@ const btnRadioFancy = document.querySelector('fancy');
 const btnRadioBoringBetty = document.querySelector('boring');
 const btnMoo = document.querySelector('moo');
 
-function bigger(number) {
+function bigger() {
   alert("Hello, world!");
-  return Math.floor(Math.random() * (number+1));
   document.getElementById("text").style.font-size = "4em";
 }
 
-function fancy(number) {
+function fancy() {
   document.getElementById("text").style.font-weight = "bold";
   document.getElementById("text").style.font-color = "#0000ff";
   document.getElementById("text").style.text-decoration = "underline";
   document.getElementById("boring").checked = "false";
 }
 
-function boring(number) {
+function boring() {
   document.getElementById("text").style.font-size = "1em";
   document.getElementById("text").style.font-color = "#000000";
   document.getElementById("text").style.font-family = "Arial";
   document.getElementById("fancy").checked = "false";
 }
 
-function moo(number) {
+function moo() {
   document.getElementById("text").style.text-transformation = "uppercase";
 }
 
