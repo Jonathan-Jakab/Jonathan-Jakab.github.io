21ea00ef2623beaf2b8b2f1a5c19f6c603d759a7
SomeOrdinaryGamer <81064130+SomeOrdinaryGamer@users.noreply.github.com>
2022-02-18 16:47:03 -0800

    Update fancifymytext.js

--- js/fancifymytext.js
+++ js/fancifymytext.js
@@ -5,25 +5,27 @@
 
 function bigger() {
   alert("Hello, world!");
-  document.getElementById("text").style.font-size = "4em";
+  document.getElementById("text").style.fontSize = "4em";
 }
 
 function fancy() {
   document.getElementById("text").style.fontWeight = "bold";
-  document.getElementById("text").style.fontColor = "#0000ff";
+  document.getElementById("text").style.color = "#0000ff";
   document.getElementById("text").style.textDecoration = "underline";
+  document.getElementById("text").style.fontFamily = "Garamond";
   document.getElementById("boring").checked = "false";
 }
 
 function boring() {
   document.getElementById("text").style.fontSize = "1em";
-  document.getElementById("text").style.fontColor = "#000000";
-  document.getElementById("text").style.fontFamily = "Arial";
+  document.getElementById("text").style.color = "#000000";
+  document.getElementById("text").style.fontStyle = "Arial";
   document.getElementById("fancy").checked = "false";
 }
 
 function moo() {
-  document.getElementById("text").style.textTransformation = "uppercase";
+  document.getElementById("text").style.textTransform = "capitalize";
+  document.getElementById("text").style.fontFamily = "Garamond";
 }
 
 // btnBigger.onclick = bigger;
