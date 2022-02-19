763a4d404b29854a0dee10c449fdade713680b9d
SomeOrdinaryGamer <81064130+SomeOrdinaryGamer@users.noreply.github.com>
2022-02-18 17:12:09 -0800

    Update fancifymytext.js

--- js/fancifymytext.js
+++ js/fancifymytext.js
@@ -3,35 +3,35 @@ function bigger() {
     alert("Hello, world!");
   }
   
-  function fancy() {  
+function fancy() {  
     let textField = document.getElementById("text");
     if(document.getElementById("fancy").checked) {
-  //     textField.style.fontSize = "12pt";
-      textField.style.fontWeight = "bold";
-      textField.style.color = "#0000ff";
-      textField.style.textDecoration = "underline";
-      textField.style.fontFamily = "Garamond";
-      document.getElementById("boring").checked = "false";
+        // textField.style.fontSize = "12pt";
+        textField.style.fontWeight = "bold";
+        textField.style.color = "#0000ff";
+        textField.style.textDecoration = "underline";
+        textField.style.fontFamily = "Garamond";
+        document.getElementById("boring").checked = "false";
     }
   }
   
   
-  function boring() {  
+function boring() {  
     let textField = document.getElementById("text");
     if(document.getElementById("boring").checked) {
-      textField.style.fontSize = "12pt";
-      textField.style.fontWeight = "normal";
-      textField.style.color = "#000000";
-      textField.style.textDecoration = "none";
-      textField.style.fontStyle = "Arial";
-      document.getElementById("fancy").checked = "false";
+        textField.style.fontSize = "12pt";
+        textField.style.fontWeight = "normal";
+        textField.style.color = "#000000";
+        textField.style.textDecoration = "none";
+        textField.style.fontStyle = "Arial";
+        document.getElementById("fancy").checked = "false";
     }
-  }
+}
   
-  function moo() {  
+function moo() {  
     let textField = document.getElementById("text");
-      textField.style.textTransform = "uppercase";
-  //     textField.style.textTransform = "capitalize";
+    textField.style.textTransform = "uppercase";
+    textField.style.textTransform = "capitalize";
     let parts = textField.value.split(".");
-      textField.value = parts.join("-Moo.");
-  }
+    textField.value = parts.join("-Moo.");
+}
