922def6dce670f0d3b19c12b95ac5d318333967d
SomeOrdinaryGamer <81064130+SomeOrdinaryGamer@users.noreply.github.com>
2022-02-18 16:41:44 -0800

    Update fancifymytext.js

--- js/fancifymytext.js
+++ js/fancifymytext.js
@@ -1,7 +1,7 @@
-const btnBigger = document.querySelector('bigger');
-const btnRadioFancy = document.querySelector('fancy');
-const btnRadioBoringBetty = document.querySelector('boring');
-const btnMoo = document.querySelector('moo');
+// const btnBigger = document.querySelector('bigger');
+// const btnRadioFancy = document.querySelector('fancy');
+// const btnRadioBoringBetty = document.querySelector('boring');
+// const btnMoo = document.querySelector('moo');
 
 function bigger() {
   alert("Hello, world!");
@@ -9,27 +9,27 @@ function bigger() {
 }
 
 function fancy() {
-  document.getElementById("text").style.font-weight = "bold";
-  document.getElementById("text").style.font-color = "#0000ff";
-  document.getElementById("text").style.text-decoration = "underline";
+  document.getElementById("text").style.fontWeight = "bold";
+  document.getElementById("text").style.fontColor = "#0000ff";
+  document.getElementById("text").style.textDecoration = "underline";
   document.getElementById("boring").checked = "false";
 }
 
 function boring() {
-  document.getElementById("text").style.font-size = "1em";
-  document.getElementById("text").style.font-color = "#000000";
-  document.getElementById("text").style.font-family = "Arial";
+  document.getElementById("text").style.fontSize = "1em";
+  document.getElementById("text").style.fontColor = "#000000";
+  document.getElementById("text").style.fontFamily = "Arial";
   document.getElementById("fancy").checked = "false";
 }
 
 function moo() {
-  document.getElementById("text").style.text-transformation = "uppercase";
+  document.getElementById("text").style.textTransformation = "uppercase";
 }
 
-btnBigger.onclick = bigger;
-btnRadioFancy.onclick = fancy;
-btnRadioBoringBetty.onclick = boring;
-btnMoo.onclick = moo;
+// btnBigger.onclick = bigger;
+// btnRadioFancy.onclick = fancy;
+// btnRadioBoringBetty.onclick = boring;
+// btnMoo.onclick = moo;
 
 // btnBigger.onclick('click', bigger);
 // btnRadioFancy.addEventListener('click', fancy);
