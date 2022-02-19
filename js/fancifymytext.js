5267bf17f4422caeadd219d7870af6dd79c12ee0
SomeOrdinaryGamer <81064130+SomeOrdinaryGamer@users.noreply.github.com>
2022-02-18 16:29:02 -0800

    Update fancifymytext.js

--- js/fancifymytext.js
+++ js/fancifymytext.js
@@ -1,3 +1,8 @@
+const btnBigger = document.querySelector('bigger');
+const btnRadioFancy = document.querySelector('fancy');
+const btnRadioBoringBetty = document.querySelector('boring');
+const btnMoo = document.querySelector('moo');
+
 function bigger(number) {
   alert("Hello, world!");
   return Math.floor(Math.random() * (number+1));
@@ -8,15 +13,22 @@ function fancy(number) {
   document.getElementById("text").style.font-weight = "bold";
   document.getElementById("text").style.font-color = "#0000ff";
   document.getElementById("text").style.text-decoration = "underline";
+  document.getElementById("boring").checked = "false";
 }
 
 function boring(number) {
   document.getElementById("text").style.font-size = "1em";
   document.getElementById("text").style.font-color = "#000000";
   document.getElementById("text").style.font-family = "Arial";
+  document.getElementById("fancy").checked = "false";
 }
 
 function moo(number) {
   document.getElementById("text").style.text-transformation = "uppercase";
 }
+
+btnBigger.addEventListener('click', bigger);
+btnRadioFancy.addEventListener('click', fancy);
+btnRadioBoringBetty.addEventListener('click', boring);
+btnMoo.addEventListener('click', moo);
 
