d8ac73e6b4940efb6e8979c5b0d988fc76312e2b
SomeOrdinaryGamer <81064130+SomeOrdinaryGamer@users.noreply.github.com>
2022-02-18 16:21:14 -0800

    Update fancifymytext.js

--- js/fancifymytext.js
+++ js/fancifymytext.js
@@ -1,1 +1,22 @@
-alert("Hello, world!");
+function bigger(number) {
+  alert("Hello, world!");
+  return Math.floor(Math.random() * (number+1));
+  document.getElementById("text").style.font-size = "4em";
+}
+
+function fancy(number) {
+  document.getElementById("text").style.font-weight = "bold";
+  document.getElementById("text").style.font-color = "#0000ff";
+  document.getElementById("text").style.text-decoration = "underline";
+}
+
+function boring(number) {
+  document.getElementById("text").style.font-size = "1em";
+  document.getElementById("text").style.font-color = "#000000";
+  document.getElementById("text").style.font-family = "Arial";
+}
+
+function moo(number) {
+  document.getElementById("text").style.text-transformation = "uppercase";
+}
+
